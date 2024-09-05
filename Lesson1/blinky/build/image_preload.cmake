88d8355bcd5817952b71ab82cb166facb748ee50
Tiago Cunha <t.cunha@swordhealth.com>
2024-09-05 17:28:45 +0100

    Deleted bonus File

--- Lesson1/blinky/build/image_preload.cmake
+++
@@ -1,6 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dk/nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "/home/sword/ncs/toolchains/e9dba88316/usr/local/bin/python3.9" CACHE INTERNAL "NCS child image controlled")
